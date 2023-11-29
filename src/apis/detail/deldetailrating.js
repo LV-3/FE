73f401431e9c759662d4805c3f8f22386e42e50f
eunhabaek <beh2549@gmail.com>
2023-11-29 17:06:46 +0900

    :rocket: 배포 테스트 종료

--- src/apis/detail/deldetailrating.js
+++ src/apis/detail/deldetailrating.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 export const delReview = async(subsr,content_id) => {
-    const result = await axios.delete(`/detail/${content_id}/rating`, {
+    const result = await axios.delete(`http://1.220.201.108:8080/detail/${content_id}/rating`, {
         data:{subsr}
     })
     return result;
 }
-
-//{ //("http://localhost:30/ratings", { 
