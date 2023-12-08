bde16fdc3deea47f5ac79e344e9bb0878215c77f
eunhabaek <beh2549@gmail.com>
2023-12-08 14:28:39 +0900

    :bug:  HTTPS 에러 해결을 위한 reload 수정

--- src/apis/main/getreload3_post.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-//VODdata 가져오기
-export const VOD_model3 = async (subsr) => {
-    const result = await axios.post('https://d225nwg9l5o274.cloudfront.net/main/reload3', {subsr}); //("http://localhost:30/mainreload3",{subsr})
-    return result;
-};
