5ea30546ed5df0df0a0a94038e300243cd60c979
eunhabaek <beh2549@gmail.com>
2023-12-11 17:21:37 +0900

    :lipstick: 네브바 CSS 일부 수정

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,11 +2,12 @@ import React from 'react'
 import '../css/Navbar.css';
 import { Link } from 'react-router-dom';
 import DropDownSubsr from '../components/DropDownSubsr';
-import DropDownGenres from '../components/DropDownGenres';
+import DropDownMovies from '../components/DropDownMovies';
 import logo from "../assets/logo2.png"
 import { useState } from 'react';
 import DropDownTv from '../components/DropDownTv';
 import DropDownKids from '../components/DropDownKids';
+import searchIcon from "../assets/search.png"
 
 
 export default function Navbar() {
@@ -25,17 +26,14 @@ export default function Navbar() {
           </Link>
         </div>
         
-        <input className="SearchInput" placeholder='     VOD제목이나 출연 배우를 검색해주세요.' onChange={onChange} value={searchText}></input>
 
           
 
           {/* <Link to='/search'> */}
            <div className="GenreMenu"> 
-            <DropDownGenres>
-              
-            </DropDownGenres>
-              {/* </Link> */}
+            <DropDownMovies/>
             </div>
+
           <div className="DropDownTv">
             <DropDownTv />
           </div>
@@ -44,10 +42,15 @@ export default function Navbar() {
             <DropDownKids />
           </div>
 
+    
+          <input className="SearchInput" placeholder='' onChange={onChange} value={searchText}></input>
+          <img src={searchIcon} className='searchIcon'></img>
+
+
+
         <div className="SubsrContainer"> 
-          <DropDownSubsr>
-            
-          </DropDownSubsr>
+          <DropDownSubsr/>
+
         </div>
     </div>
   )
