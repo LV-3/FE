4bfc9a4d0411f6a8fd881c36214a82841151736b
kangansoo <ansoo971@gmail.com>
2023-12-18 13:51:47 +0900

    :lipstick: 디테일 페이지 정렬, 배너 데이터 파일 생성

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import lgevent from '../assets/lgevent.png'
+import lginternet from '../assets/lginternet.png'
+import lgmarket from '../assets/lgmarket.png'
+import lgmobile from '../assets/lgmobile.png'
+import lgrental from '../assets/lgrental.png'
+
+export const banner = [
+                        {bannerimg: lgevent, bannerurl: 'https://www.lghellovision.net/benefits/event/eventMain.do?Ls_Type=0'},
+                        {bannerimg: lginternet, bannerurl: 'https://www.lghellovision.net/product/internet/internetPriceList.do'},
+                        {bannerimg: lgmarket, bannerurl: 'https://seasonmarket.co.kr/'},
+                        {bannerimg: lgmobile, bannerurl: 'https://direct.lghellovision.net/main.do'},
+                        {bannerimg: lgrental, bannerurl: 'https://rental.lghellovision.net/main/detail?utm_source=TPS_HP&utm_medium=GNB_shop_rental&utm_campaign=TPS_HP&utm_content=GNB_shop_rental&_ga=2.188265005.449452101.1702788871-963828872.1700788453&_gac=1.83537252.1702819740.CjwKCAiA1fqrBhA1EiwAMU5m__UdpLOai7CgWnirAk_--ucTzijnQgImezdmiBcGLAK24qKROO3GPxoC_TcQAvD_BwE'}
+                    ] 
+
