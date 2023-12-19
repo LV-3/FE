1955a57a7b91f45821ae47b76c30868fd980adb4
kangansoo <ansoo971@gmail.com>
2023-12-19 14:06:39 +0900

    :goal_net: 마이페이지 리플레이 에러 처리 및 메인 오류 수정

--- src/reducer/ReplayReducer.js
+++ src/reducer/ReplayReducer.js
@@ -1,12 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getReplay } from '../apis/mypage/getmypagereplay_post';
 
-export const getReplays = createAsyncThunk("GetReplay", async (subsr)=>{
+export const getReplays = createAsyncThunk("GetReplay", async (subsr, { rejectWithValue })=>{
     try{
         const result = await getReplay(subsr);
         return result.data
     } catch (error){
         console.log('Replay Error', error);
+        return rejectWithValue(error.response.status);
     }
 })
 
@@ -14,13 +15,17 @@ const replaySlice = createSlice({
     name: "Replays",
     initialState: {
         vodData: [],
+        error: null,
     },
     reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(getReplays.fulfilled, (state, action)=>{
                 state.vodData = action.payload;
-                state.error = 0;
+                state.error = null;
+            })
+            .addCase(getReplays.rejected, (state, action)=>{
+                state.error = action.payload;
             })
     },
 });
