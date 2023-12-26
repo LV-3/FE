2d878824a6bdd860e99565fc6607fcac2bece2d9
kangansoo <ansoo971@gmail.com>
2023-12-26 12:47:36 +0900

    :lipstick: 메인페이지 배너 추가

--- src/css/StyledComponents.js
+++ src/css/StyledComponents.js
@@ -352,11 +352,12 @@ export const DImgLabel = styled.label`
 `;
 
 export const DPoster = styled.img`
-    display: flex;
-    border-color: black;
-    border-radius: 3px; 
-    justify-content: center;
-    object-fit: cover;
+  display: flex;
+  flex: 0 1 0;
+  border-color: transparent;
+  height: 100%;
+  border-radius: 3px; 
+  justify-content: stretch;
 `;
 
 
