0c43a1bdbbdb3ed7e33a82e584f6448f9431d2c4
eunhabaek <beh2549@gmail.com>
2023-12-30 17:05:43 +0900

    :bricks: API 변경

--- src/apis/detail/gettags.js
+++ src/apis/detail/gettags.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getTags = async (content_id)=>{
-    const result = await axios.get(`https://api.hellogptv.com/detail/${content_id}/tags`) 
+    const result = await axios.get(`https://d225nwg9l5o274.cloudfront.net/${content_id}/tags`) 
     return result;
 }
