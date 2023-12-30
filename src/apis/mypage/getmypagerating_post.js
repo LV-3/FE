bfe95f820b6b88ad43ba27421594a815fbb2e850
eunhabaek <beh2549@gmail.com>
2023-12-30 16:39:37 +0900

    :bricks: Api 주소 EC2로 변경

--- src/apis/mypage/getmypagerating_post.js
+++ src/apis/mypage/getmypagerating_post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getmypagerating = async (subsr) => {
-    const result = await axios.post('https://d225nwg9l5o274.cloudfront.net/mypage/rating', {subsr});
+    const result = await axios.post('https://api.hellogptv.com/mypage/rating', {subsr});
     return result;
 };
