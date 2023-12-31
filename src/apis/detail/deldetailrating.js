3bc43d5f9cfc5a0380209fe00c2631f123bc8d86
eunhabaek <beh2549@gmail.com>
2023-12-31 11:33:39 +0900

    :bricks: API 설정 변경

--- src/apis/detail/deldetailrating.js
+++ src/apis/detail/deldetailrating.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export const delReview = async(subsr,content_id) => {
-    const result = await axios.delete(`https://d225nwg9l5o274.cloudfront.net/${content_id}/rating`, {
+    const result = await axios.delete(`https://hellogptv.com/${content_id}/rating`, {
         data:{subsr}
     })
     return result;
 }
+// https://hellogptv.com
 // https://d225nwg9l5o274.cloudfront.net
 // http://lv3-loadbalancer-918926550.ap-northeast-2.elb.amazonaws.com
 // http://localhost:30
-// https://d225nwg9l5o274.cloudfront.net
+// https://api.hellogptv.com
