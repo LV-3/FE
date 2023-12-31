0f7a8c6bc3a6ef69626e94c555eb2dd7b048e95f
eunhabaek <beh2549@gmail.com>
2023-12-31 10:48:41 +0900

    :bricks: API 변경

--- src/apis/detail/deldetailrating.js
+++ src/apis/detail/deldetailrating.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const delReview = async(subsr,content_id) => {
-    const result = await axios.delete(`https://api.hellogptv.com/${content_id}/rating`, {
+    const result = await axios.delete(`https://d225nwg9l5o274.cloudfront.net/${content_id}/rating`, {
         data:{subsr}
     })
     return result;
 }
-// https://api.hellogptv.com
+// https://d225nwg9l5o274.cloudfront.net
 // http://lv3-loadbalancer-918926550.ap-northeast-2.elb.amazonaws.com
 // http://localhost:30
 // https://d225nwg9l5o274.cloudfront.net
--- src/apis/genres/getTvGenreList.js
+++ src/apis/genres/getTvGenreList.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //genre별 VOD 리스트 가져오기
 export const tvGenreList = async (genre1) => {
-    const result = await axios.get(`https://api.hellogptv.com/tv/${genre1.replaceAll('/', ':')}`);
+    const result = await axios.get(`https://d225nwg9l5o274.cloudfront.net/tv/${genre1.replaceAll('/', ':')}`);
     return result;
 };
