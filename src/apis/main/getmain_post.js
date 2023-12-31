b37e1641362b31de37c0759910a97d8a88f9a166
eunhabaek <beh2549@gmail.com>
2023-12-31 19:16:38 +0900

    :bricks: API 변경

--- src/apis/main/getmain_post.js
+++ src/apis/main/getmain_post.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 //VODdata 가져오기
 export const allVods = async (subsr) => {
-    const result = await axios.post('https://hellogptv.com/main', {subsr}); 
+    const result = await axios.post('https://d225nwg9l5o274.cloudfront.net/main', {subsr}); 
     return result;
 };
