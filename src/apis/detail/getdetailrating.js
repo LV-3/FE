cada1d1dfc1b52ec063c0051652f7bc2479320a2
eunhabaek <beh2549@gmail.com>
2024-01-13 18:22:22 +0900

    :bug: 디테일페이지 API 요청경로 수정

--- src/apis/detail/getdetailrating.js
+++ src/apis/detail/getdetailrating.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getratingdata = async(content_id) => {
-    const response = await axios.get(`https://d3l7tgeznk7sw9.cloudfront.net/${content_id}/rating`)
+    const response = await axios.get(`https://d3l7tgeznk7sw9.cloudfront.net/detail/${content_id}/rating`)
     return response;
 }
