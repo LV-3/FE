0d93e192096438a0e84fcddb10444dcd1f94b9f8
eunhabaek <beh2549@gmail.com>
2024-01-13 16:54:52 +0900

    :bug: 디테일 페이지 API 요청 경로 에러 수정

--- src/apis/detail/putdetailrating.js
+++ src/apis/detail/putdetailrating.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const putrating = async(content_id, subsr, rating, review, rating_date)=> {
-    const result=await axios.put(`https://d3l7tgeznk7sw9.cloudfront.net/${content_id}/rating`, 
+    const result=await axios.put(`https://d3l7tgeznk7sw9.cloudfront.net/detail/${content_id}/rating`, 
     {
         subsr: subsr,
         rating: rating,
